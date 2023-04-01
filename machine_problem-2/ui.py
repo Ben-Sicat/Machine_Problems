f7688407d3ae48c713e4933fdffefb6b86b209ee
Ben-Sicat <bensicat00@gmail.com>
2023-04-02 00:43:33 +0800

    started the ui with tkinter

--- machine_problem-2/ui.py
+++ machine_problem-2/ui.py
@@ -1,8 +1,51 @@
 import tkinter as tk
+from main import *
+
+equation = " "
+obj = Calculator(equation)
+
+def add_to_calculation(text):
+    global equation
+    equation += str(text)
+    text_result.delete("1.0", tk.END)
+    text_result.insert(tk.END, equation)
+    
+def evaluate():
+    global equation
+    try:
+        result = obj.bisection(equation, 0, 1)
+        text_result.delete("1.0", tk.END)
+        text_result.insert(tk.END, result)
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")
+
+def clear_field():
+    global equation
+    equation = ""
+    text_result.delete("1.0", tk.END)
 
 root = tk.Tk()
 root.geometry("500x480")
 
 text_result = tk.Text(root, height=2, width=30, font =("Times New Roman", 24) )
 text_result.grid(columnspan=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 root.mainloop()
